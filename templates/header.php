2842b0bf43da97ed95d432d81d548feba580b619
georgevfc13 <georgeyeste@gmail.com>
2025-11-23 14:17:40 -0500

    headerheader

---
+++ templates/header.php
@@ -0,0 +1,10 @@
+<header class="main-header">
+    <nav class="navbar">
+        <ul class="nav-list">
+            <li class="nav-item"><a href="#" data-section="inicio" class="nav-link">Inicio</a></li>
+            <li class="nav-item"><a href="#" data-section="notas" class="nav-link">Notas</a></li>
+            <li class="nav-item"><a href="#" data-section="sobre-nosotros" class="nav-link">Sobre Nosotros</a></li>
+            <li class="nav-item"><a href="#" data-section="ubicacion" class="nav-link">Ubicación</a></li>
+        </ul>
+    </nav>
+</header>
